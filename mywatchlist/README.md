b4dc8187c91e11195bca2ae478ed3a0b4b8248e1
hyperpulsor <rakanfasya@gmail.com>
2022-09-21 01:21:57 +0700

    Feat : Added bonus implementation and Fix Heroku

---
+++ mywatchlist/README.md
@@ -0,0 +1,6 @@
+# Tugas 3 Pengimplementasian Data Delivery Menggunakan Django
+Rakan Fasya Athhar Rayyan - 2106750950
+
+[Link *Web App* HTML]()\
+[Link *Web App* XML]()
+
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,9 +6,31 @@ from mywatchlist.models import FilmWatch
 # Create your views here.
 def show_films(request):
     data_film = FilmWatch.objects.all()
-    context = {
-        'list_film' : data_film
-    }
+
+    sum_watched = 0
+    sum_not_watched = 0
+
+    for films in data_film :
+        if (films.watched):
+            sum_watched += 1
+        else:
+            sum_not_watched += 1
+
+    if (sum_watched >= sum_not_watched):
+        context = {
+        'list_film' : data_film,
+        'bonus' : "Selamat, kamu sudah banyak menonton!",
+        'sum_watched' : sum_watched,
+        'sum_not_watched' : sum_not_watched
+        }
+    else :
+        context = {
+        'list_film' : data_film,
+        'bonus' : "Wah, kamu masih sedikit menonton!",
+        'sum_watched' : sum_watched,
+        'sum_not_watched' : sum_not_watched
+        }
+    
     return render(request, "mywatchlist.html", context)
 
 def show_films_xml(request):
